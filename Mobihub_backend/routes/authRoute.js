70bf81ce7966b55dec3f60f2887b901535e131af
Pankesh Meena <pankeshmeena977@gamil.com>
2024-03-07 17:27:14 +0530

    some changese

--- Mobihub_backend/routes/authRoute.js
+++ Mobihub_backend/routes/authRoute.js
@@ -1,28 +1,19 @@
 
-=======
 const express = require("express");
-const { createUser, loginUserCtrl, getalluser, getauser, getauserdelete, getauserupdated, ManageAddress } = require("../controller/userCtrl");
-const { ProductAdd, DisplayProduct, DeleteProduct } = require("../controller/AdminControler")
-const { setemail } = require("../controller/sendemail");
-
 const router = express.Router();
 const {createUser,loginUserCtrl,getalluser,getauser, getauserdelete,getauserupdated,ManageAddress} = require("../controller/userCtrl");
-const {ProductAdd,DisplayProduct} = require("../controller/AdminControler");
-// const {setemail} = require("../controller/sendemail");
-
-// Routes for user management
+const {ProductAdd,DisplayProduct,DeleteProduct, BrandController,CouponControler} = require("../controller/AdminControler");
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
 router.get('/all_users', getalluser);
 router.get('/:id', getauser);
 router.delete('/:id', getauserdelete);
 router.put('/:id', getauserupdated);
-
-=======
 router.post('/ProdoctAdd', ProductAdd);
 router.post('/DisplayProduct', DisplayProduct);
 router.delete('/DeleteProduct/:id', DeleteProduct);
-
 router.post('/ManagedlyAddress', ManageAddress);
+router.post('/Brandctrl', BrandController);
+router.post('/Couponctrl', CouponControler);
 module.exports = router;
 
