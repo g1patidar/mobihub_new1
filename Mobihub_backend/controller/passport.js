cc7edb616ba9bb9a50706316555b444551b97441
Pankesh Meena <112115701+PankeshMeena1@users.noreply.github.com>
2024-03-13 14:38:24 +0530

    Update passport.js

--- Mobihub_backend/controller/passport.js
+++ Mobihub_backend/controller/passport.js
@@ -1,20 +1,20 @@
 // const passport = require ("../config/passwordconfig");
 
-const userdb = require("../models/user/loginwithgogl");
+// const userdb = require("../models/user/loginwithgogl");
 
-const getUserData = async (req, res) => {
-    try {
-        const userData = await userdb.findOne({ googleId: req.user.googleId });
-        console.log(userData, "pankesh");
-        res.status(200).json(userData);
-    } catch (error) {
-        console.error("Error fetching user data:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-};
+// const getUserData = async (req, res) => {
+//     try {
+//         const userData = await userdb.findOne({ googleId: req.user.googleId });
+//         console.log(userData, "pankesh");
+//         res.status(200).json(userData);
+//     } catch (error) {
+//         console.error("Error fetching user data:", error);
+//         res.status(500).json({ message: "Internal server error" });
+//     }
+// };
 
-module.exports={
-    getUserData,
-}
+// module.exports={
+//     getUserData,
+// }
 
 
