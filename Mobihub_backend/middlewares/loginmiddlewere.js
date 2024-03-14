c3c55829a2159d709b7cebde56a4f6e85f052f31
jeevan patidar <142888747+g1patidar@users.noreply.github.com>
2024-03-14 12:23:40 +0530

    Update loginmiddlewere.js

--- Mobihub_backend/middlewares/loginmiddlewere.js
+++ Mobihub_backend/middlewares/loginmiddlewere.js
@@ -1,10 +1,1 @@
-// const isAuthenticated = (req, res, next) => {
-//     if (req.isAuthenticated()) {
-//         return next();
-//     }
-//     res.status(401).json({ message: "Unauthorized" });
-// };
 
-// module.exports={
-//     isAuthenticated,
-// }
